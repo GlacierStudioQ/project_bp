d9d890576201b779ffd141c8d08b6e5a444c6f13
qiaojuyang <rache21431@outlook.com>
2018-05-01 21:38:49 +0800

    去掉日志

--- src/bipubipu/util/utils.go
+++ src/bipubipu/util/utils.go
@@ -3,7 +3,6 @@ package util
 import (
 	"strings"
 	"reflect"
-	"fmt"
 )
 
 //获取某结构体中某字段的某Tag的值，如果没有则返回空字符串
@@ -19,7 +18,6 @@ func GetStructTagName(structName interface{}, fieldName string, tagKey string) (
 	// 遍历字段
     fieldNum := t.NumField()  
     for i := 0; i < fieldNum; i++ {
-        fmt.Println("field name = " + t.Field(i).Name)
 		if(0 == strings.Compare(fieldName, t.Field(i).Name)){
             tagVal = t.Field(i).Tag.Get(tagKey)
             return tagVal
